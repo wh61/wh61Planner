bda09472a7c1c0623bd22ac7c505d7b2308b2e3d
Wu Hong <wuhong@WudeMacBook-Pro.local>
2016-01-16 02:26:58 +0800

    delete something

--- wh61Planning-fmyedzcpvghswugjylhklshvwswp/Build/Intermediates/wh61Planning.build/Debug/wh61Planning.build/Objects-normal/x86_64/mapbuilder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/wuhong/Projects/wh61Planning/wh61Planning/mapbuilder.cpp \
-  /Users/wuhong/Projects/wh61Planning/wh61Planning/mapbuilder.hpp \
-  /Users/wuhong/Projects/wh61Planning/wh61Planning/datastructs.h
